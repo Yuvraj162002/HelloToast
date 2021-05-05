72551441154cc9316e91b2c0d92c384e421d7284
Yuvraj162002 <77117240+Yuvraj162002@users.noreply.github.com>
2021-05-05 13:02:01 +0530

    Update Horizontal.java

--- app/src/main/java/com/example/toastapp/Horizontal.java
+++ app/src/main/java/com/example/toastapp/Horizontal.java
@@ -9,20 +9,22 @@ import android.widget.Toast;
 import com.example.toastapp.databinding.ActivityMainBinding;
 
 public class Horizontal extends AppCompatActivity {
+          //Create Quantity...
           private int quantity = 0;
            private ActivityMainBinding b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+              ///Initiliaze the Activity Binding..
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
     }
-
+///Create Toast
     public void TOAST(View view) {
         Toast.makeText(this, "Hello word", Toast.LENGTH_SHORT).show();
     }
-
+///Increment Quantity..
     public void qty(View view) {
         b.qty.setText(""+quantity++);
     }
