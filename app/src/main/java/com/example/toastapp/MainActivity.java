5c4c113a5fdca22b70b87e63cdff12bef84c1815
Yuvraj162002 <77117240+Yuvraj162002@users.noreply.github.com>
2021-05-05 12:59:59 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/toastapp/MainActivity.java
+++ app/src/main/java/com/example/toastapp/MainActivity.java
@@ -9,23 +9,27 @@ import android.widget.Toast;
 import com.example.toastapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+             //Create quantity..
             private int quantity = 0;
             private ActivityMainBinding c;
 
     @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
+                        
+                 ///Initialize the activity binding..
                 c = ActivityMainBinding.inflate(getLayoutInflater());
-                // setContentView(R.layout.activity_main);
+                
                 setContentView(c.getRoot());
             }
 
+              // Create Toast..
             public void TOAST(View view) {
 
                 Toast.makeText(this, "Hello toast", Toast.LENGTH_SHORT).show();
 
             }
-
+                 ///Increment the quantity..
             public void qty(View view) {
                c.qty.setText(""+quantity++);
 
